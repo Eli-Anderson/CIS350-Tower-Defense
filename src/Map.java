43b571f14e27ee8d0715f14a8e4ed361a12e9779
ultrasnap <ultrasnap@yahoo.com>
2018-02-14 14:55:43 -0500

    Fixed README.md headers' formatting wrong.

--- src/Map.java
+++ src/Map.java
@@ -9,22 +9,97 @@ public class Map {
         for (int y=0; y<height; y++) {
             for (int x=0; x<width; x++) {
                 tiles[y][x] = new Tile(x, y);
+
             }
         }
     }
-
+    public int getWidth () {
+        return this.width;
+    }
+    public int getHeight () {
+        return this.height;
+    }
     Tile getTile (int x, int y) {
         return this.tiles[y][x];
     }
 
-    void createPath () {
-        
+    public void createPath (int col, int row) {
+        createPath(col, row, null);
+    }
+    private void createPath (int col, int row, Tile lastTilePlaced) {
+        System.out.println(this);
+        if (col >= getWidth()) {
+            return;
+        }
+        boolean canGoUp = true;
+        boolean canGoDown = true;
+        if (lastTilePlaced != null) {
+            if (lastTilePlaced.type == 'u') {
+                // last tile was going up, so we can't go down (no overwriting)
+                canGoDown = false;
+            } else if (lastTilePlaced.type == 'd') {
+                // last tile was going down, so we can't go up
+                canGoUp = false;
+            }
+        }
+
+        char[] options = {'r','r','r'};
+        if (row <= 0) {
+            // at the top of the map
+            if (canGoDown) {
+                options[1] = 'd';
+                options[2] = 'd';
+            }
+        } else {
+            // not at the top
+            if (row >= getHeight() - 1) {
+                // at the bottom
+                if (canGoUp) {
+                    options[1] = 'u';
+                    options[2] = 'u';
+                }
+            } else {
+                // in the middle somewhere
+                if (canGoUp) {
+                    options[1] = 'u';
+                }
+                if (canGoDown) {
+                    options[2] = 'd';
+                }
+            }
+        }
+
+
+        int r = (int) Math.floor(Math.random() * 3);
+
+        tiles[row][col].type = options[r];
+        switch (options[r]) {
+            case 'u':
+                createPath(col, row-1, tiles[row][col]);
+                break;
+            case 'r':
+                createPath(col+1, row, tiles[row][col]);
+                break;
+            case 'd':
+                createPath(col, row+1, tiles[row][col]);
+                break;
+        }
     }
 
-    int getWidth () {
-        return this.width;
+    public String toString () {
+        StringBuilder result = new StringBuilder();
+        for (int col = 0; col < tiles.length; col++) {
+            for (int row = 0; row < tiles[col].length; row++) {
+                result.append(tiles[col][row].type);
+            }
+            result.append("\n");
+        }
+        return result.toString();
     }
-    int getHeight () {
-        return this.height;
+
+    public static void main(String[] args) {
+        Map m = new Map(8,8);
+        m.createPath(0,3);
+        System.out.println(m);
     }
 }
--- src/Tile.java
+++ src/Tile.java
@@ -1,7 +1,9 @@
 public class Tile {
     private int x, y;
+    public char type;
     Tile (int x, int y) {
         this.x = x;
         this.y = y;
+        this.type = '•';
     }
 }
