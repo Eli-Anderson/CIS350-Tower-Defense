fc1575dec0a1a73cb95412943dcdefedefcc12e6
JerryYK <31463326+JerryYK@users.noreply.github.com>
2018-02-14 01:47:06 -0500

    updated the tower class and add some new method

--- src/Tower.java
+++ src/Tower.java
@@ -1,18 +1,26 @@
 /*****************************************************************
- * CIS 250 Group Project Tower Defense Tower class and its methods
+ * CIS 350 Group Project Tower Defense Tower class and its methods
  * @author: Runquan Ye 
  * @version: T01
  * @date: winter/2018 
  *****************************************************************/
-public class tower {
+public class Tower {
+	/**get connect to the monster class*/
+	private Monster target;
 	/** X is for Tower's position */
 	private int towerX;
 	
 	/** Y is for Tower's position */
 	private int towerY;
 	
+	/** The distence between tower and monster */
+	private int distance;
+	
 	/** Tower's attack value */
 	private int attackValue;
+	
+	/** Tower's attack Speed */
+	private double attackSpeed;
 	
 	/** Tower's attack range */
 	private int attackRange;
@@ -20,36 +28,67 @@ public class tower {
 	/** Tower's building cost */
 	private int buildingFee;
 	
+	/** get the monster health from monster class */
+	private int monsterHealth;
+	
+	
 	/** Tower's type (Rock, Paper, Scissor) different type will affect the damage to the monsters*/
 	private Type towerType;
 	
+	/** To check should the tower attack */
+	private boolean attack = false;
+	
 	//constructor
-	public tower(int towerX, int towerY, Type towerType, int buildingFee){
+	public Tower(int towerX, int towerY, Type towerType){
 		this.towerX = towerX;
 		this.towerY = towerY;
 		this.towerType = towerType;
-		this.buildingFee = buildingFee;
-		
 	}
 
 	public void typeofTower(Type touwerTyper){
+		//Rock type tower: expensive, low attack speed, small attack range, but high attack value.
 		if(towerType == Type.Rock){
 			attackRange = 3;
-			attackValue = 50;
+			attackValue = 125;
+			buildingFee = 100;
+			attackSpeed = 1;
 		}
+		
+		//Paper type tower: all the feature are neutral.
 		if(towerType == Type.Paper){
 			attackRange = 4;
 			attackValue = 40;
+			buildingFee = 75;
+			attackSpeed = 1.5;
 		}
+		
+		//Scissor type tower: cheap, fast attack speed, large attack range, but very low attack value.
 		if(towerType == Type.Scissor){
 			attackRange = 5;
 			attackValue = 30;
+			buildingFee = 50;
+			attackSpeed = 2.5;
 		}
 			
 	}
 	
-	public void radar(){
+	public void radar(int targetX, int targetY){
+		// get monster's location
+		//calculate the distance between the tower and monster
+		distance = 0;
 		
+		if(distance < getAttackRange()){
+			attack = true;
+		}
+	}
+	
+	
+	
+	public void fire(Type touwerTyper, Type MonsterType){
+		attack(touwerTyper, MonsterType);
+		if(attack == true){
+			monsterHealth -= attackValue * attackSpeed;
+		}
 		
 	}
 	
@@ -58,37 +97,37 @@ public class tower {
 		if(towerType == Type.Rock){
 			switch(MonsterType){
 			case Rock:
-				attackValue = 50;
+				break;
 			
 			case Paper:
-				attackValue = 35;
+				attackValue -= 15;
 			
 			case Scissor:
-				attackValue = 65;
+				attackValue += 15;
 			}
 		}
 		if(towerType == Type.Paper){
 			switch(MonsterType){
 			case Rock:
-				attackValue = 55;
+				attackValue += 15;
 			
 			case Paper:
-				attackValue = 40;
+				break;
 			
 			case Scissor:
-				attackValue = 25;
+				attackValue -= 15;
 			}
 		}
 		if(towerType == Type.Scissor){
 			switch(MonsterType){
 			case Rock:
-				attackValue = 15;
+				attackValue -= 15;
 			
 			case Paper:
-				attackValue = 45;
+				attackValue += 15;
 			
 			case Scissor:
-				attackValue = 30;
+				break;
 			}
 		}
 		
@@ -115,34 +154,23 @@ public class tower {
 		return attackValue;
 	}
 
-	public void setAttackValue(int attackValue) {
-		this.attackValue = attackValue;
-	}
 
 	public int getAttackRange() {
 		return attackRange;
 	}
 
-	public void setAttackRange(int attackRange) {
-		this.attackRange = attackRange;
+	public double getAttackSpeed() {
+		return attackSpeed;
 	}
 
 	public int getBuildingfee() {
-		return buildingfee;
-	}
-
-	public void setBuildingfee(int buildingfee) {
-		this.buildingfee = buildingfee;
+		return buildingFee;
 	}
 
 	public Type getTowerType() {
 		return towerType;
 	}
 
-	public void setTowerType(Type towerType) {
-		this.towerType = towerType;
-	}
-	
 	
 	
 }
