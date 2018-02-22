2fc4c3b19e7c66b9be9a230f3a187ce0d55d5697
ultrasnap <ultrasnap@yahoo.com>
2018-02-21 21:05:27 -0500

    Updated Map
    createPath now creates a randomized path from left to right (no backwards movement yet)
    
    Map now prints unicode characters for each tile, properly drawing how the map should look in the GUI
    
    Created addTower and getTower methods

--- src/Map.java
+++ src/Map.java
@@ -1,17 +1,26 @@
 public class Map {
     private int width, height;
     private Tile[][] tiles;
+    private Tower[][] towers;
+    private static final char R = '\u21E2';
+    private static final char U = '\u21E1';
+    private static final char D = '\u21E3';
+    private static final char UR = '\u21B1';
+    private static final char DR = '\u21B3';
+    private static final char RD = '\u21B4';
+    private static final char RU = '\u2197';
 
     Map (int width, int height) {
         this.width = width;
         this.height = height;
-        tiles = new Tile[height][width];
+        this.tiles = new Tile[height][width];
         for (int y=0; y<height; y++) {
             for (int x=0; x<width; x++) {
                 tiles[y][x] = new Tile(x, y);
-
             }
         }
+        this.createPath(0, height / 2);
+        this.towers = new Tower[this.height][this.width];
     }
     public int getWidth () {
         return this.width;
@@ -23,48 +32,50 @@ public class Map {
         return this.tiles[y][x];
     }
 
-    public void createPath (int col, int row) {
+    private void createPath (int col, int row) {
         createPath(col, row, null);
+        System.out.println(this);
+        fixCorners();
+
     }
     private void createPath (int col, int row, Tile lastTilePlaced) {
-        System.out.println(this);
         if (col >= getWidth()) {
             return;
         }
         boolean canGoUp = true;
         boolean canGoDown = true;
         if (lastTilePlaced != null) {
-            if (lastTilePlaced.type == 'u') {
+            if (lastTilePlaced.type == U) {
                 // last tile was going up, so we can't go down (no overwriting)
                 canGoDown = false;
-            } else if (lastTilePlaced.type == 'd') {
+            } else if (lastTilePlaced.type == D) {
                 // last tile was going down, so we can't go up
                 canGoUp = false;
             }
         }
 
-        char[] options = {'r','r','r'};
+        char[] options = {R,R,R};
         if (row <= 0) {
             // at the top of the map
             if (canGoDown) {
-                options[1] = 'd';
-                options[2] = 'd';
+                options[1] = D;
+                options[2] = D;
             }
         } else {
             // not at the top
             if (row >= getHeight() - 1) {
                 // at the bottom
                 if (canGoUp) {
-                    options[1] = 'u';
-                    options[2] = 'u';
+                    options[1] = U;
+                    options[2] = U;
                 }
             } else {
                 // in the middle somewhere
                 if (canGoUp) {
-                    options[1] = 'u';
+                    options[1] = U;
                 }
                 if (canGoDown) {
-                    options[2] = 'd';
+                    options[2] = D;
                 }
             }
         }
@@ -73,17 +84,70 @@ public class Map {
         int r = (int) Math.floor(Math.random() * 3);
 
         tiles[row][col].type = options[r];
-        switch (options[r]) {
-            case 'u':
+
+        switch (tiles[row][col].type) {
+            case U:
                 createPath(col, row-1, tiles[row][col]);
                 break;
-            case 'r':
+            case R:
                 createPath(col+1, row, tiles[row][col]);
                 break;
-            case 'd':
+            case D:
                 createPath(col, row+1, tiles[row][col]);
                 break;
         }
+    }
+
+    private void fixCorners () {
+        // start at middle
+        int currentX = 0;
+        int currentY = this.height / 2;
+        Tile lastTile = this.tiles[currentY][currentX];
+
+        while (currentX < this.width) {
+            System.out.println(lastTile.type);
+            switch (lastTile.type) {
+                case U:
+                case RU:
+                    currentY--;
+                    if (this.tiles[currentY][currentX].type == R)
+                        this.tiles[currentY][currentX].type = UR;
+                    break;
+                case D:
+                case RD:
+                    currentY++;
+                    if (this.tiles[currentY][currentX].type == R)
+                        this.tiles[currentY][currentX].type = DR;
+                    break;
+                case R:
+                case UR:
+                case DR:
+                    currentX++;
+                    if (currentX >= this.width)
+                        return;
+
+                    if (this.tiles[currentY][currentX].type == U)
+                        this.tiles[currentY][currentX].type = RU;
+                    else if (this.tiles[currentY][currentX].type == D)
+                        this.tiles[currentY][currentX].type = RD;
+                    break;
+            }
+            lastTile = this.tiles[currentY][currentX];
+        }
+
+    }
+
+    public void addTower (Tower tower, int x, int y) {
+        if (x < this.width && x >= 0 &&
+            y < this.height && y >= 0) this.towers[y][x] = tower;
+        //else
+            // out of bounds
+    }
+
+    public Tower getTower (int x, int y) {
+        if (x < this.width && x >= 0 &&
+            y < this.height && y >= 0) return this.towers[y][x];
+        return null;
     }
 
     public String toString () {
@@ -99,7 +163,6 @@ public class Map {
 
     public static void main(String[] args) {
         Map m = new Map(8,8);
-        m.createPath(0,3);
         System.out.println(m);
     }
 }
