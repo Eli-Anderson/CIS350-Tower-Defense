f5bf62c8be13b568ddf62fc224af9c75fe4cb277
kzaroukian <zaroukik@mail.gvsu.edu>
2018-03-01 18:40:19 -0500

    update
    Update

--- src/Monster.java
+++ src/Monster.java
@@ -88,76 +88,7 @@ public class Monster {
         return reward;
     }
     
-    // idk if this would in theory work, would have to have a different multiplier
-    public int battle(Type tower, Type monster){
-        if(monster == Type.Rock){
-            if(tower == Rock){
-                // no effect?
-                health += 1;
-            }
-            if(tower == Paper){
-                attack += 15;
-            }
-            
-            if(tower == Scissor){
-                attack -= 15;
-            }
-        }
-        if(monster == Type.Paper){
-            if(tower == Rock){
-                attack += 10;
-            }
-            if(tower == Paper){
-                // no effect?
-                health += 1;
-            }
-            
-            if(tower == Scissor){
-                attack -= 25;
-            }
-        }
-        if(monster == Type.Scissor){
-            if(tower == Rock){
-                attack -= 20;
-            }
-            if(tower == Paper){
-                attack += 25;
-            }
-            
-            if(tower == Scissor){
-                // no effect?
-                health += 1;
-            }
-        }
-        return health;
-    }
-    
-    // TODO: Make SubMonster classes?
-    
-    public void monsterType(Type monster){
-        if(monster == Type.Rock){
-            attack = 100;
-            health = 1000;
-            moveSpeed = 5;
-            attackSpeed = 4;
-            //range = 10;
-        }
-        if(monster == Type.Paper){
-            attack = 50;
-            health = 750;
-            moveSpeed = 3;
-            attackSpeed = 2;
-            //range = 4;
-        }
-        if(monster == Type.Scissor){
-            attack = 200;
-            health = 1500;
-            moveSpeed = 12;
-            attackSpeed = 10;
-            //range = 6;
-            
-        }
-    }
+
     
     // TODO: add function to locate monster's exact position as well as tower's exact position
     
